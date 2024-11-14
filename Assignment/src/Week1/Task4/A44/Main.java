9b078916d591c42fc70a065bd7324795e052b07d
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-11-15 00:36:26 +0200

    Week1, Task 4: A44. Create a pseudo code for the simulation

---
+++ Assignment/src/Week1/Task4/A44/Main.java
@@ -0,0 +1,32 @@
+package Week1.Task4.A44;
+
+public class Main {
+    /*
+    simulation
+    initial()
+    - set up inital state, events and eventlist
+    - add initial events to event list
+    - set simulation = true, set conditon when simulation is false
+    run()
+    - while (simulation)
+        - tryphaseA()
+            - get the first event in the eventList
+            - get the event's time and advance the clock to this event's time
+        -tryphaseB()
+            - execute all B-event happen in this time
+        -tryphaseC()
+            - proceed to attemp any C-Event in this timestamp
+                - if there is any C-Event happened, reattemp all the C-events in this timestamp
+            - if there is no C-Event perform, continue,C-Event complete
+        -checkSimulation()
+            - check if the condition of the timestamp can satisfy the condition to set simulation to false
+            - if not, proceed to the next iteration
+            - if yes, get out of this loop
+
+    - stop()
+
+
+
+     */
+
+}
