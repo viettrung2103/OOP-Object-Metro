d4d49df24fe2c3c8dfd74b18a0f2df2aa48db2b6
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-11-14 16:53:30 +0200

    Week1, Task 4: A41

---
+++ Lesson/Week4/src/distribution/ContinuousGenerator.java
@@ -0,0 +1,7 @@
+package distribution;
+
+/** A continuous generator provides a double value according to the distribution it relies on.
+ */
+public interface ContinuousGenerator extends Seedable {
+    double sample(); 
+}
